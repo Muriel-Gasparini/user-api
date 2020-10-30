5f77293df50bcbc447f99256239cae3cca6037ce
Muriel Gasparini <muriel.developer@gmail.com>
2020-10-30 16:54:30 -0300

    test

--- src/main/java/com/user/api/mongo/UserApiApplication.java
+++ src/main/java/com/user/api/mongo/UserApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class main {
+public class UserApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(main.class, args);
+        SpringApplication.run(UserApiApplication.class, args);
     }
 
 }
