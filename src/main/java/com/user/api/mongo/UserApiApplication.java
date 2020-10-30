c40200d29ec0c8b995966a5863a6e5e2642bd0e4
Muriel Gasparini <muriel.developer@gmail.com>
2020-10-30 15:49:18 -0300

    refactor: add config

--- src/main/java/com/user/api/mongo/UserApiApplication.java
+++ src/main/java/com/user/api/mongo/UserApiApplication.java
@@ -2,20 +2,12 @@ package com.user.api.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
 
-@Controller
 @SpringBootApplication
-public class ApiApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
+public class UserApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(UserApiApplication.class, args);
     }
+
 }
