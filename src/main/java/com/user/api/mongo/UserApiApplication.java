d8e164e643551203b5b39fd06a6ba04454892832
Muriel Gasparini <muriel.developer@gmail.com>
2020-10-30 15:41:38 -0300

    add

--- src/main/java/com/user/api/mongo/UserApiApplication.java
+++ src/main/java/com/user/api/mongo/UserApiApplication.java
@@ -2,12 +2,20 @@ package com.user.api.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
-public class UserApiApplication {
+public class ApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApiApplication.class, args);
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
